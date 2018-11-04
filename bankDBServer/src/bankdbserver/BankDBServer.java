3c1ec741138afd8a0d90baa15c1a69fbf27d8495
Omar M. Adel <eng.omaradel95@yahoo.com>
2018-11-04 22:07:07 +0200

    adding sql stuff
    i forgot to save the file on the IDE so it didn't update :smile:

--- bankDBServer/src/bankdbserver/BankDBServer.java
+++ bankDBServer/src/bankdbserver/BankDBServer.java
@@ -10,6 +10,9 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,10 +23,12 @@ class clientHandler implements Runnable
 {
 
     Socket c;
+    Connection myConn;
 
-    public clientHandler(Socket c)
+    public clientHandler(Socket c,Connection myConn)
     {
         this.c = c;
+        this.myConn=myConn;
     }
 
     @Override
@@ -39,6 +44,34 @@ class clientHandler implements Runnable
             {
                 //Handle the requests from the Logic server to store and
                 //retrieve data from the database.
+                Statement myStmt = null;
+                ResultSet myRs = null;
+                try {
+                    // 2. Create a statement
+                    myStmt = myConn.createStatement();
+
+                    // 3. Execute SQL query
+                    myRs = myStmt.executeQuery("select * from employees");
+
+                    // 4. Process the result set
+                    while (myRs.next()) {
+                        System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+                    }
+                } catch (Exception exc) {
+                    exc.printStackTrace();
+                } finally {
+                    if (myRs != null) {
+                        myRs.close();
+                    }
+
+                    if (myStmt != null) {
+                        myStmt.close();
+                    }
+
+                    if (myConn != null) {
+                        myConn.close();
+                    }
+                }
             }
 
             //5. close comm with client
@@ -70,13 +103,24 @@ public class BankDBServer {
             //2.accept
             //3.create socket (I/O) with client
             ServerSocket s = new ServerSocket(2222);
+            Connection myConn = null;
+            
+            try {
+                // 1. Get a connection to database
+                myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbName", "dbUsername",
+                        "dbPassword");
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(BankDBServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
             while (true)
             {
+                
                 
                 Socket c = s.accept();
                 
                 System.out.println("Logic server Arrived!");
-                clientHandler ch = new clientHandler(c);
+                clientHandler ch = new clientHandler(c,myConn);
                 //ch.run();//wait till run finish its code
 
                 //handle client in parrallel
